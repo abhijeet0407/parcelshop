1dcac8d5bd2b43be8ab7356bf00c476fcd492cb8
abhijeet0407 <abhijeet@digitalvibe.in>
2016-12-24 14:01:54 +0530

    new mobile view5

--- resources/views/parcel/mobileparcellist.php
+++ resources/views/parcel/mobileparcellist.php
@@ -4,7 +4,7 @@
 	<tr> 
     <td align="center" valign="top"><?php echo $i; ?></td>
     <td align="left" valign="top" class="parcel-code"><?php echo $parcel->id ?></td>
-    <td align="left" valign="top"><?php echo $parcel->parceldata->address ?></td>
+    <td align="left" valign="top"><?php //echo $parcel->parceldata->address ?></td>
         
 	</tr>
 
