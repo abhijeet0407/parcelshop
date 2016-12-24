cb75cf642feaaa436497366c8c67ff5df08424eb
abhijeet0407 <abhijeet@digitalvibe.in>
2016-12-24 14:08:40 +0530

    new mobile view6

--- resources/views/parcel/mobileparcellist.php
+++ resources/views/parcel/mobileparcellist.php
@@ -1,10 +1,10 @@
-<?php $i=0; foreach($parcel_data as $parcel){ ?>
+<?php $i=1; foreach($parcel_data as $parcel){ ?>
 
 	
 	<tr> 
     <td align="center" valign="top"><?php echo $i; ?></td>
     <td align="left" valign="top" class="parcel-code"><?php echo $parcel->id ?></td>
-    <td align="left" valign="top"><?php //echo $parcel->parceldata->address ?></td>
+    <td align="left" valign="top"><?php echo $parcel->parceldata['address'] ?></td>
         
 	</tr>
 
