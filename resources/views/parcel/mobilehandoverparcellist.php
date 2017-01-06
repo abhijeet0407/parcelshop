4e0f923dab9b2b5ef6acffdfc59ffab1869770e9
abhijeet0407 <abhijeet@digitalvibe.in>
2017-01-06 18:57:18 +0530

    twak handover8

--- resources/views/parcel/mobilehandoverparcellist.php
+++ resources/views/parcel/mobilehandoverparcellist.php
@@ -1,4 +1,22 @@
 <?php $i=1; foreach($parcel_data as $parcel){
+  if($parcel->customer['account_type']=='Prepaid Customer'){
+    if(($parcel->parceldata['recipient_name']!='') && ($parcel->parceldata['zipcode']!='') && ($parcel->parceldata['address']!='')  && ($parcel->parceldata['phone']!='') ){
+   ?>
+
+        <tr> 
+          <td align="center" valign="top"><?php echo $i; ?></td>
+          <td align="left" valign="top" class="parcel-code"><?php echo $parcel->parceltoken ?></td>
+          <td align="left" valign="top"><?php echo $parcel->parceldata['recipient_name'] ?><?php echo $parcel->parceldata['address'] ?></td>
+          <td align="center" valign="top">
+            <input type="checkbox" value="<?php echo $parcel->id ?>"  name="handover[]" >
+              
+          </td>
+        </tr>
+
+
+ <?php
+    } 
+}else{
 	if(($parcel->parceldata['recipient_name']!='') && ($parcel->parceldata['zipcode']!='') && ($parcel->parceldata['address']!='')  && ($parcel->parceldata['phone']!='') && ($parcel->parceldata['producttype']!='') && ($parcel->parceldata['destination']!='') && ($parcel->parceldata['price']!='') ){
  ?>
 
@@ -9,12 +27,13 @@
     <td align="left" valign="top"><?php echo $parcel->parceldata['recipient_name'] ?><?php echo $parcel->parceldata['address'] ?></td>
     <td align="center" valign="top">
       <input type="checkbox" value="<?php echo $parcel->id ?>"  name="handover[]" >
-      
+        
     </td>
 	</tr>
 
 
-<?php  $i++;
+<?php  
+  }
 }
-
+$i++;
  } ?>
