2f313cd3a52926939aff5f86b4d3f39a935f2b26
abhijeet0407 <abhijeet@digitalvibe.in>
2017-01-06 17:23:41 +0530

    twak handover2

--- resources/views/parcel/mobilehandoverparcellist.php
+++ resources/views/parcel/mobilehandoverparcellist.php
@@ -7,10 +7,10 @@
     <td align="center" valign="top"><?php echo $i; ?></td>
     <td align="left" valign="top" class="parcel-code"><?php echo $parcel->parceltoken ?></td>
     <td align="left" valign="top"><?php echo $parcel->parceldata['recipient_name'] ?><?php echo $parcel->parceldata['address'] ?></td>
-    <td align="center" valign="top"><div class="squaredTwo">
+    <td align="center" valign="top">
       <input type="checkbox" value="<?php echo $parcel->id ?>"  name="handover[]" >
-      <label for="squaredTwo"></label>
-    </div></td>
+      
+    </td>
 	</tr>
 
 
